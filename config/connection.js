682479400c22b485cab033923da2303ac229486a
Katlin <katjohnson926@yahoo.com>
2019-01-15 15:17:51 -0500

    heroku changes

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
-// var mysql = require('mysql');
-// var connection;
+var mysql = require('mysql');
+var connection;
 
-// if(proccess.env.JAWSDB_URL){
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }else {
-//        // Database is local
-//     connection = mysql.createConnection({
-//         port: 3306,
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'password',
-//         database: 'bamazon'
-//     });
-// };
+if (proccess.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'Charlotte215!',
+        database: 'bamazon'
+    });
+};
 
-// connection.connect();
-// module.exports = connection;
+connection.connect();
+module.exports = connection;
